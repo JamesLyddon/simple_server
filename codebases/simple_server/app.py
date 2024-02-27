3f7c14675ca53dcf73edf82ddd6db750d1da9e13
JamesLyddon <jameslyddon@gmail.com>
2024-02-27 10:57:46 +0000

    fixed deployment bug

--- codebases/simple_server/app.py
+++ codebases/simple_server/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def get_home():
-    return render_template('welcome.html')
+    return render_template("welcome.html")
 
 if __name__ == '__main__':
     app.run(
